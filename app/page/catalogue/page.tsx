010f3c2ef5fcea8c4afbb7c15c607e1939871ebb
Salwa-pratama <nandev1915@gmail.com>
2025-12-21 21:14:15 +0700

    setup awal brai

---
+++ app/page/catalogue/page.tsx
@@ -0,0 +1,11 @@
+import { memo } from "react";
+
+const Catalogue = () => {
+  return (
+    <div>
+      <h2>Catalogue</h2>
+    </div>
+  );
+};
+
+export default memo(Catalogue);
---
+++ components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import { memo } from "react";
+
+const Header = () => {
+  return (
+    <header className="flex">
+      <h2>logo</h2>
+    </header>
+  );
+};
+
+export default memo(Header);
